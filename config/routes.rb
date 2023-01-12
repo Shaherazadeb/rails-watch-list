a47a44310c69f5307f048eb4e1698557e88a033d
Shaherazade Bendjelloun <shaherazadeb@gmail.com>
2023-01-12 20:08:14 +0100

    rails watch list

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'lists/new'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to 'lists#index'
+  get "lists", to: "lists#index"
+  get "lists/:id", to: "lists#show"
+  get "lists/new", to: "lists#new"
+  post "lists", to: "lists#create"
+end
