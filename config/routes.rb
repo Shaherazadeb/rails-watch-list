f32ec20ca59affcb10cc6bb3a32d005a88fe5c09
Shaherazade Bendjelloun <shaherazadeb@gmail.com>
2023-01-13 13:06:39 +0100

    update rails watch list

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root to: 'lists#index'
-  get "lists", to: "lists#index"
-  get "lists/:id", to: "lists#show"
-  get "lists/new", to: "lists#new"
-  post "lists", to: "lists#create"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
